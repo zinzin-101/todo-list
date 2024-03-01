3c6c15f7130d868b37cdd3a291719f427fa75d51
zinzin-101 <mr.punthep@gmail.com>
2024-03-01 16:38:29 +0700

    added css and load/save functionality

--- src/App.js
+++ src/App.js
@@ -1,40 +1,59 @@
 import React, {useState} from "react";
+import "./App.css"
 
 function App() {
 
-  const [list, setList] = useState([]);
+  const [itemList, setList] = useState([]);
   const [input, setInput] = useState("");
 
+  React.useEffect(() => {
+    const jsonFile = localStorage.getItem("todo-list");
+    const parsedJson = JSON.parse(jsonFile);
+
+    console.log("start");
+
+    if (parsedJson){
+      setList(parsedJson);
+    }
+  }, [])
+
+  React.useEffect(() => {
+    const jsonFile = JSON.stringify(itemList);
+    console.log("loaded");
+    localStorage.setItem("todo-list", jsonFile);
+  }, [itemList])
+
   const addItem = (item) => {
+    if (item === "") return;
     const newItem = { // json object
       id: Math.random(), //random key for mapping
       item: item
     };
     
-    setList([...list, newItem]); //add item to the list, the '...' means append
+    setList([...itemList, newItem]); //add item to the list, the '...' means append
     setInput(""); //clear input box
   };
 
   const deleteItem = (id) => {
-    const newList = list.filter((item) => item.id !== id); //create a new list that filters out an item with the input id
+    const newList = itemList.filter((item) => item.id !== id); //create a new list that filters out an item with the input id
     setList(newList);
   }
 
   return (
     <div>
-      <h1>Todo List</h1>
-      <input type="text" 
+      <h1><span id="header">Stylish</span><br></br>Todo<span id="textgradient">List</span></h1>
+      <input id="inputbox" type="text" 
       value={input} 
       onChange={(e) => setInput(e.target.value)} //listen for input and update setInput
       />
-      <button onClick={() => addItem(input)}>Add</button>
-      <ul>
+      <button id="addbutton" onClick={() => addItem(input)}>Add</button>
+      <ul id="itemcontainer">
         {
-          list.map((item) => (
-            <li key={item.id}>
+          itemList.map((item) => (
+            <ul id="itembox" key={item.id}>
               {item.item}
-              <button onClick={() => deleteItem(item.id)}>&times;</button>
-            </li>
+              <button id="removebutton" onClick={() => deleteItem(item.id)}>&times;</button>
+            </ul>
           ))
         }
       </ul>
